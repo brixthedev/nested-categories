174dce68d1a6e2c6b977162010cdb6135444e452
brixTheDev <76054416+brixthedev@users.noreply.github.com>
2021-05-31 01:54:02 +0800

    Update App.js
    1. Refactored return statement
    2. Exported staticCategories to a different file.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
 import { useState, useEffect } from "react";
 
 import RenderCategories from "./renderCategories";
-
-const staticCategories = [
-  {
-    name: "root",
-    categories: [
-      {
-        name: "1",
-        categories: [
-          {
-            name: "1.1",
-            categories: [],
-          },
-          {
-            name: "1.2",
-            categories: [
-              {
-                name: "1.2.1",
-                categories: [],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        name: "2",
-        categories: [
-          {
-            name: "2.1",
-            categories: [],
-          },
-        ],
-      },
-    ],
-  },
-];
+import staticCategories from "./staticCategories";
 
 function App() {
   const [nestedCategories, setNestedCategories] = useState([]);
